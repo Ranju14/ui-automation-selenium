49952a0b8c4c927406c05505b06d1cba6c517cc5
Ranjani Raveendra <ranjani14.kr@gmail.com>
2019-06-17 02:02:20 +1000

    -fixed wrong import name

--- src/test/java/com/services/nsw/pages/HomePageNSW.java
+++ src/test/java/com/services/nsw/pages/HomePageNSW.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import utils.BaseClass;
+import com.services.nsw.utils.BaseClass;
 
 public class HomePageNSW extends BaseClass {
 	
